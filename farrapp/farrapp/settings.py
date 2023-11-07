378851bd19b3b2f3c22734fb21984cbd495f17fe
Paola Gallegos <54852214+PaolaGAPI@users.noreply.github.com>
2023-11-07 17:28:10 -0500

    s3 connection

--- farrapp/farrapp/settings.py
+++ farrapp/farrapp/settings.py
@@ -85,6 +85,8 @@ config = {
  #       "NAME": BASE_DIR / "db.sqlite3",
  #   }
 #}
+ 
+# DATABASE RDS
 
 DATABASES = {
     "default": {
@@ -136,3 +138,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#Bucket s3 Image Storage
+
+AWS_ACCESS_KEY_ID = config['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = config['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = 'superflowerpowers3'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'us-west-2'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
