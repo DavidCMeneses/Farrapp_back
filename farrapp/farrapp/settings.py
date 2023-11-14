01dcddeb5f4d34a4562496cd334cbf455fdf24b1
David Garzón <halodaga@gmail.com>
2023-11-13 22:21:27 -0500

    Update: Allowed hosts

--- farrapp/farrapp/settings.py
+++ farrapp/farrapp/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = "django-insecure-8vpnrxz9ln&u=1fh)gj+x+du5ak3o1-4$-@4=%6u**t!ssb@xt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.2.2','127.0.0.1']
 
 # Application definition
 
