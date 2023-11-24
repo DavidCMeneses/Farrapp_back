b7ac83d4b1a56c75c6366e92a989c9918973a6ae
David Casallas <61563462+DavidCMeneses@users.noreply.github.com>
2023-11-23 19:24:51 -0500

    Created the fetch function
    Yes

--- farrapp/api/urls.py
+++ farrapp/api/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('update_pref/<str:user_type>/', views.update_preferences, name="api-update-preferences"),
     path('delete_user/<str:user_type>/', views.delete_user, name="api-delete-user"),
     path('rate/', views.rate, name = "api-rate"),
+    path('fetch_info/<str:establishment_id>', views.fetch_establishment_info, name = "api-fetch-info")
 ]
